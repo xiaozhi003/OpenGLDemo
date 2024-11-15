0482a3e13b68b6c9f7312deae0c9ada9d68225e6
zhi.wang <zhi.wang@opay-inc.com>
2024-11-15 16:19:58 +0800

    添加README.md

---
+++ README.md
@@ -0,0 +1,24 @@
+# OpenGLDemo
+
+本项目是Android下OpenGL ES2.0学习的样例代码
+
+## Blog
+
+[Android OpenGLES开发：EGL环境搭建](https://blog.csdn.net/xiaozhiwz/article/details/141868444)
+
+[Android OpenGLES2.0开发（一）：艰难的开始](https://blog.csdn.net/xiaozhiwz/article/details/142354149)
+
+[Android OpenGLES2.0开发（二）：环境搭建](https://blog.csdn.net/xiaozhiwz/article/details/142366766)
+
+[Android OpenGLES2.0开发（三）：绘制一个三角形](https://blog.csdn.net/xiaozhiwz/article/details/142453506)
+
+[Android OpenGLES2.0开发（四）：矩阵变换和相机投影](https://blog.csdn.net/xiaozhiwz/article/details/142488394)
+
+[Android OpenGLES2.0开发（五）：绘制正方形和圆形](https://blog.csdn.net/xiaozhiwz/article/details/142530158)
+
+[Android OpenGLES2.0开发（六）：着色器语言GLSL](https://blog.csdn.net/xiaozhiwz/article/details/142790866)
+
+[Android OpenGLES2.0开发（七）：纹理贴图之显示图片](https://blog.csdn.net/xiaozhiwz/article/details/142871148)
+
+[Android OpenGLES2.0开发（八）：Camera预览](https://blog.csdn.net/xiaozhiwz/article/details/143239446)
+
--- app/src/main/java/com/android/xz/opengldemo/gles/draw/CameraFilter.java
+++ app/src/main/java/com/android/xz/opengldemo/gles/draw/CameraFilter.java
@@ -33,17 +33,12 @@ public class CameraFilter {
 
     // 顶点着色器代码
     private final String vertexShaderCode =
-            // This matrix member variable provides a hook to manipulate
-            // the coordinates of the objects that use this vertex shader
             "uniform mat4 uMVPMatrix;\n" +
                     "uniform mat4 uTexPMatrix;\n" +
                     "attribute vec4 vPosition;\n" +
                     "attribute vec4 vTexCoordinate;\n" +
                     "varying vec2 aTexCoordinate;\n" +
                     "void main() {\n" +
-                    // the matrix must be included as a modifier of gl_Position
-                    // Note that the uMVPMatrix factor *must be first* in order
-                    // for the matrix multiplication product to be correct.
                     "  gl_Position = uMVPMatrix * vPosition;\n" +
                     "  aTexCoordinate = (uTexPMatrix * vTexCoordinate).xy;\n" +
                     "}";
